1115e0ce240da1a59933bbd0d4beefc39c256bf1
Strasen <Christian.Strasen@thomsonreuters.com>
2019-01-11 00:05:50 +0000

    fixing tests for PATCH /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -149,72 +149,40 @@ describe('DELETE /todos/:id', () => {
 
 describe('PATCH /todos/:id', () => {
   it('should update the todo', (done) => {
-<<<<<<< HEAD
-    const hexId = todos[0]._id.toHexString();
-    const text = 'This should be the new text';
+    var hexId = todos[0]._id.toHexString();
+    var text = 'This should be the new text';
 
-=======
-    // grab ID of first item 
-    const hexId = todos[0]._id.toHexString();
-    //update text, set completed equal to true
-    const text = 'This is the new text';
-    //assert 200 back
->>>>>>> 95fcacabce91be46fec983a7edb5104650e1f97e
     request(app)
       .patch(`/todos/${hexId}`)
       .send({
         completed: true,
         text
       })
-<<<<<<< HEAD
-=======
-       // text is changed, complted is true, and completed at is a num
->>>>>>> 95fcacabce91be46fec983a7edb5104650e1f97e
       .expect(200)
       .expect((res) => {
         expect(res.body.todo.text).toBe(text);
         expect(res.body.todo.completed).toBe(true);
         expect(res.body.todo.completedAt).toBeA('number');
       })
-      .end(done)
-<<<<<<< HEAD
-  })
-
-  it('should clear completedAt when todo is not completed', (done) => {
-    const hexId = todos[1]._id.toHexString();
-    const text = 'This should be text';
-
-=======
-   
+      .end(done);
   });
 
   it('should clear completedAt when todo is not completed', (done) => {
-    const hexId = todos[1]._id.toHexString();
-    //update text, set completed equal to true
-    const text = 'This is the text for second case';
-    //assert 200 back
->>>>>>> 95fcacabce91be46fec983a7edb5104650e1f97e
+    var hexId = todos[1]._id.toHexString();
+    var text = 'This should be the new text!!';
+
     request(app)
       .patch(`/todos/${hexId}`)
       .send({
         completed: false,
         text
       })
-<<<<<<< HEAD
-=======
-       // text is changed, complted is true, and completed at is a num
->>>>>>> 95fcacabce91be46fec983a7edb5104650e1f97e
       .expect(200)
       .expect((res) => {
         expect(res.body.todo.text).toBe(text);
         expect(res.body.todo.completed).toBe(false);
         expect(res.body.todo.completedAt).toNotExist();
       })
-      .end(done)
-<<<<<<< HEAD
-  })
-})
-=======
+      .end(done);
   });
 });
->>>>>>> 95fcacabce91be46fec983a7edb5104650e1f97e
